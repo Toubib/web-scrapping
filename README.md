c5c8ca3d519e789907a6bf43d2fbaa7fa14884ce
Vincent Reydet <toubib.lag@gmail.com>
2015-08-03 16:34:16 +0200

    add ac script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web-scrapping
+
+Miscellaneous scrapping scripts
---
+++ ac/ac.js
@@ -0,0 +1,35 @@
+moment = require('moment.min.js');
+
+var casper = require('casper').create({
+    verbose: true,
+	logLevel: "info",
+});
+
+var result_path = 'public';
+
+casper.start('https://www.airchina.fr/FR/FR/Home');
+
+casper.then(function() {
+    this.fill('form.flight-search-form', {
+		FSB1FromSource: 'Paris , Aéroport International Charles De Gaulle   (CDG), France',
+		B_LOCATION_1: 'CDG',
+		FSB1ToDestination: 'Shenyang, Aéroport International Taoxian  (SHE), Chine',
+		E_LOCATION_1: 'SHE',
+		B_DATE_1: '17/10/2015',
+		B_DATE_2: '30/10/2015',
+		NB_ADT: '2',
+		NB_CHD: '1',
+		NB_INF: '1'
+		//CABIN: 'E'
+	}, true);
+});
+
+casper.waitForUrl('https://www.airchina.fr/CAOnline/dyn/air/booking/availability',function() {
+	this.captureSelector(result_path+'/'+moment().format('YYYY-MM-DD') + '.png','div.page-container');
+});
+
+casper.on('error', function(error) {
+	    this.log(error);
+});
+
+casper.run();
